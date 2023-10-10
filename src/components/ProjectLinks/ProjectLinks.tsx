539c9ef5583293d0fd58117ff3a762155ad5070b
emilvn <emilvnielsen@hotmail.com>
2023-10-10 13:49:26 +0200

    added project links

--- src/components/ProjectLinks/ProjectLinks.tsx
+++ src/components/ProjectLinks/ProjectLinks.tsx
@@ -1,12 +1,20 @@
 import {ReactElement} from "react";
 import {TiWeatherPartlySunny} from "react-icons/ti";
 import "./ProjectLinks.css";
+import {MdOutlineLibraryMusic} from "react-icons/md";
+import {FaPersonSwimming} from "react-icons/fa6";
 
 export default function ProjectLinks():ReactElement {
     return (
         <div className={"Project-links"}>
             <a href="https://emilvn.github.io/weather-app" target="_blank" rel="noreferrer">
                 <TiWeatherPartlySunny/><span>Weather App</span>
+            </a>
+            <a href="https://emilvn.github.io/musicbase-frontend/" target="_blank" rel="noreferrer">
+                <MdOutlineLibraryMusic/><span>Musicbase</span>
+            </a>
+            <a href="https://emilvn.github.io/delfinen/">
+                <FaPersonSwimming/><span>Swimming club</span>
             </a>
         </div>
     );
