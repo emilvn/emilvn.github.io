c1418ca37ec103f07a6e4617ff8e289e490d16c3
emilvn <emilvnielsen@hotmail.com>
2023-10-13 09:39:20 +0200

    changed picture

--- src/components/PortfolioHeader/PortfolioHeader.tsx
+++ src/components/PortfolioHeader/PortfolioHeader.tsx
@@ -4,7 +4,7 @@ import "../../styles/PortfolioHeader.css";
 export default function PortfolioHeader():ReactElement{
     return (
         <header className={"Portfolio-header"}>
-            <img src="/assets/images/328489084_480124824329170_5589001845441032345_n.jpg" alt=""/>
+            <img src="/assets/images/me.jpg" alt=""/>
             <h1>Emil Vennervald Nielsen</h1>
         </header>
     );
