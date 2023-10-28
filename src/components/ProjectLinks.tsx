98285eba5216c6d8af6dccd6b7cd3aab701b3ab1
emilvn <emilvnielsen@hotmail.com>
2023-10-28 21:30:42 +0200

    change links

--- src/components/ProjectLinks.tsx
+++ src/components/ProjectLinks.tsx
@@ -2,13 +2,31 @@ import {ReactElement} from "react";
 import {TiWeatherPartlySunny} from "react-icons/ti";
 import "../styles/ProjectLinks.css";
 import {MdOutlineLibraryMusic} from "react-icons/md";
-import {FaPersonSwimming} from "react-icons/fa6";
 import {GrEmoji} from "react-icons/gr";
 import ReactGA from "react-ga";
 
 export default function ProjectLinks():ReactElement {
     return (
         <div className={"Project-links"}>
+            <div className={"Project-link-div"}>
+                <a href="https://tweemoji-two.vercel.app/"
+                   target="_blank"
+                   rel="noreferrer"
+                   onClick={() => {
+                       ReactGA.event({
+                           category: "Project",
+                           action: "Clicked on Tweemoji link"
+                       });
+                   }}
+                >
+                    <GrEmoji/> <span>Tweemoji</span>
+                </a>
+                <div className={"Project-link-description"}>
+                    <p>Emoji only social media page. Web app hosted on Vercel, made using the
+                        <a href={"https://create.t3.gg/"} className="inline-link">T3</a>
+                        Next.js stack. Database hosted on Planetscale, made using Prisma ORM and MySQL.</p>
+                </div>
+            </div>
             <div className={"Project-link-div"}>
                 <a
                     href="https://emilvn.github.io/weather-app"
@@ -44,42 +62,6 @@ export default function ProjectLinks():ReactElement {
                     <p>Frontend made with React and Typescript.
                     API made with NodeJS and express, hosted on Azure.
                     Database made with MySQL, hosted on Azure.</p>
-                </div>
-            </div>
-            <div className={"Project-link-div"}>
-                <a href="https://emilvn.github.io/delfinen/"
-                   target="_blank"
-                   rel="noreferrer"
-                   onClick={() => {
-                      ReactGA.event({
-                          category: "Project",
-                          action: "Clicked on Delfinen link"
-                      });
-                   }}
-                >
-                    <FaPersonSwimming/><span>Swim-club</span>
-                </a>
-                <div className={"Project-link-description"}>
-                    <p>Admin page for a swim club. Frontend made with vanilla JS/HTML/CSS. Backend implemented on Firebase using Realtime Database.</p>
-                </div>
-            </div>
-            <div className={"Project-link-div"}>
-                <a href="https://tweemoji-two.vercel.app/"
-                   target="_blank"
-                   rel="noreferrer"
-                   onClick={() => {
-                       ReactGA.event({
-                           category: "Project",
-                           action: "Clicked on Tweemoji link"
-                       });
-                   }}
-                >
-                    <GrEmoji/> <span>Tweemoji</span>
-                </a>
-                <div className={"Project-link-description"}>
-                    <p>Emoji only Twitter clone I am working on. Web app hosted on Vercel, made using the
-                        <a href={"https://create.t3.gg/"} className="inline-link">T3</a>
-                        Next.js stack. Database hosted on Planetscale, made using Prisma ORM and MySQL.</p>
                 </div>
             </div>
         </div>
