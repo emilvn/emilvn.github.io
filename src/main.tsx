5334219a604f2ef95b7abad514be85af9664b854
emilvn <emilvnielsen@hotmail.com>
2024-11-06 18:57:04 +0100

    update portfolio

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from './pages/Home.tsx';
-import './styles/index.css';
-import Layout from "./layout/Layout.tsx";
-import Projects from "./pages/Projects.tsx";
-import ReactGA from 'react-ga';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./pages/Home.tsx";
+import "./styles/index.css";
+import ReactGA from "react-ga";
 
 const trackingId = "G-CQ9XS5M7DS";
 ReactGA.initialize(trackingId);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-      <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Layout />} >
-                <Route index element={<Home />} />
-            </Route>
-        </Routes>
-      </BrowserRouter>
-  </React.StrictMode>,
-)
+    <Home />
+  </React.StrictMode>
+);
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,49 @@
-import {ReactElement} from 'react'
-import '../styles/Home.css'
-import PortfolioHeader from "../components/PortfolioHeader.tsx";
-import PortfolioLinks from "../components/PortfolioLinks.tsx";
+import { FaFile, FaGithub, FaLinkedin } from "react-icons/fa";
 
-export default function Home():ReactElement {
-
+export default function Home() {
   return (
-    <div className={"Portfolio"}>
-        <PortfolioHeader />
-        <PortfolioLinks />
+    <div
+      className={
+        "flex justify-center items-center h-screen w-screen bg-cover bg-no-repeat bg- bg-[url('/assets/images/landscape-tree-nature-forest-wilderness-branch-838595-pxhere.com.jpg')]"
+      }
+    >
+      <div className="relative overflow-hidden rounded w-96 h-[600px] bg-top bg-cover bg-no-repeat bg-[url('/assets/images/me.jpg')]">
+        <div className="absolute bottom-0 w-full bg-white bg-opacity-50 backdrop-blur-sm p-2">
+          <h1 className="text-2xl p-4 font-semibold text-center">
+            Emil Vennervald Nielsen
+          </h1>
+          <div className={"flex w-full justify-between p-4"}>
+            <a
+              href="https://www.linkedin.com/in/emil-nielsen-48b259266/"
+              target="_blank"
+              rel="noreferrer"
+              className="flex flex-col items-center text-xl [&>*]:hover:text-white"
+              title="View my LinkedIn profile"
+            >
+              <FaLinkedin className="text-5xl text-blue-600" />{" "}
+              <span>LinkedIn</span>
+            </a>
+            <a
+              href="https://www.github.com/emilvn"
+              target="_blank"
+              rel="noreferrer"
+              className="flex flex-col items-center text-xl [&>*]:hover:text-white"
+              title="View my Github profile"
+            >
+              <FaGithub className="text-5xl" /> <span>Github</span>
+            </a>
+            <a
+              href="/assets/documents/CV.pdf"
+              target="_blank"
+              rel="noreferrer"
+              className="flex flex-col items-center text-xl [&>*]:hover:text-white"
+              title="View my CV"
+            >
+              <FaFile className="text-5xl text-stone-600" /> <span>CV</span>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
